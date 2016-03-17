a574e72cf2df39d182acea7393813a3e3ccdfe03
vitaliypenkov <vitaliy.penkov@gmail.com>
2016-03-17 15:43:34 +0200

    The first test. Enabling FirefoxDriver and TestNg
    The first test. Enabling FirefoxDriver and TestNg

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by vitaliyp on 3/17/2016.
+ */
+public class SimpleTest {
+    FirefoxDriver driver;
+
+    @Test
+    public void GoogleSearch(){
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.google.com.ua/");
+        driver.findElement(By.name("q")).sendKeys("Github");
+        driver.findElement(By.name("btnG")).click();
+        String result =  driver.findElement(By.xpath(".//*[@class='rc']//a")).getText();
+
+        Assert.assertEquals(result, "How people build software · GitHub");
+    }
+
+    @AfterTest
+    public void TearDown(){
+        driver.close();
+    }
+}

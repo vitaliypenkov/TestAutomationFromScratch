d066495b772413e7bbd7099ffd6fa6c9d87e5dc1
vitaliypenkov <vitaliy.penkov@gmail.com>
2016-05-21 19:25:32 -0400

    Update SimpleTest.java

--- src/test/java/setup/webdriver/testng/SimpleTest.java
+++ src/test/java/setup/webdriver/testng/SimpleTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
